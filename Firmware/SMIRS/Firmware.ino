80758876b5e3ad75c44f14e94b38b45a566c5887
jrstrath <kelly.mulumbi@strathmore.edu>
2018-09-29 21:35:34 +0300

    Firmware slightly modified inline with project

--- Firmware/SMIRS/Firmware.ino
+++ Firmware/SMIRS/Firmware.ino
@@ -47,131 +47,46 @@ void setup()
 // Main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop.
 void loop() 
 {
-    
-    
-    if(menuOption == '1') {
-    // DHT22/11 Humidity and Temperature Sensor - Test Code
-    // Reading humidity in %
-    float dhtHumidity = dht.readHumidity();
-    // Read temperature in Celsius, for Fahrenheit use .readTempF()
-    float dhtTempC = dht.readTempC();
-    Serial.print(F("Humidity: ")); Serial.print(dhtHumidity); Serial.print(F(" [%]\t"));
-    Serial.print(F("Temp: ")); Serial.print(dhtTempC); Serial.println(F(" [C]"));
+    //Connect Ethernet Module: Look into having it in the setup instead of Loop section
 
-    }
-    else if(menuOption == '2') {}
-    // Disclaimer: The Ethernet LAN Network Module - ENC28J60 is in testing and/or doesn't have code, therefore it may be buggy. Please be kind and report any bugs you may find.
-    else if(menuOption == '3') {
-    // Soil Moisture Sensor - Test Code
-    int soilMoisture5vVal = soilMoisture5v.read();
-    Serial.print(F("Val: ")); Serial.println(soilMoisture5vVal);
 
-    }
-    else if(menuOption == '4') {
-    // 12V Solenoid Valve - 3/4'' - Test Code
-    // The solenoid valve will turn on and off for 500ms (0.5 sec)
-    solenoidValve.on(); // 1. turns on
-    delay(500);       // 2. waits 500 milliseconds (0.5 sec). Change the value in the brackets (500) for a longer or shorter delay in milliseconds.
-    solenoidValve.off();// 3. turns off
-    delay(500);       // 4. waits 500 milliseconds (0.5 sec). Change the value in the brackets (500) for a longer or shorter delay in milliseconds.
+    //Sensing all environmental conditions
+        // DHT22/11 Humidity and Temperature Sensor Reading humidity in % and Temperature in Celcius
+        float dhtHumidity = dht.readHumidity();
+        float dhtTempC = dht.readTempC();
 
-    }
-    else if(menuOption == '5') {}
-    // Disclaimer: The Water Flow Sensor G1/2'' is in testing and/or doesn't have code, therefore it may be buggy. Please be kind and report any bugs you may find.
+        //Soil moisture sensor readings
+        int soilMoisture5vVal = soilMoisture5v.read();
+
+    //Display readings
+        Serial.print(F("Humidity: ")); Serial.print(dhtHumidity); Serial.print(F(" [%]\t"));
+        Serial.print(F("Temp: ")); Serial.print(dhtTempC); Serial.println(F(" [C]"));
+        Serial.print(F("Val: ")); Serial.println(soilMoisture5vVal);
     
     
-    
-    if (millis() - time0 > timeout)
-    {
-        menuOption = menu();
-    }
-    
+    //Turn on solenoid valve depending on sensor readings.
+        /*Special Note:
+            This should use AI to change the values.
+
+            Test conditions
+            
+            read soil moisture and compare to optimum value from AI
+            25 being the placeholder
+        */
+        while(soilMoisture5vVal < 25 && dhtHumidity < 75 && dhtTempC < 25){
+        /*Special Note:
+            Use AI to determine all values 
+        */
+        
+            //Turn on
+                solenoidValve.on();
+
+            //Read flow rate to determine how much water is used to irrigate at a certain point
+            //Start timer
+            //Multiply timer by fow rate to give volume of water used
+        }
+        
+        //Turn off
+            solenoidValve.off();
 }
 
-
-
-// Menu function for selecting the components to be tested
-// Follow serial monitor for instrcutions
-char menu()
-{
-
-    Serial.println(F("\nWhich component would you like to test?"));
-    Serial.println(F("(1) DHT22/11 Humidity and Temperature Sensor"));
-    Serial.println(F("(2) Ethernet LAN Network Module - ENC28J60"));
-    Serial.println(F("(3) Soil Moisture Sensor"));
-    Serial.println(F("(4) 12V Solenoid Valve - 3/4''"));
-    Serial.println(F("(5) Water Flow Sensor G1/2''"));
-    Serial.println(F("(menu) send anything else or press on board reset button\n"));
-    while (!Serial.available());
-
-    // Read data from serial monitor if received
-    while (Serial.available()) 
-    {
-        char c = Serial.read();
-        if (isAlphaNumeric(c)) 
-        {
-            if(c == '1') 
-    			Serial.println(F("Now Testing DHT22/11 Humidity and Temperature Sensor"));
-    		else if(c == '2') 
-    		{
-    			Serial.println(F("Now Testing Ethernet LAN Network Module - ENC28J60 - note that this component doesn't have a test code"));			
-    		else if(c == '3') 
-    			Serial.println(F("Now Testing Soil Moisture Sensor"));
-    		else if(c == '4') 
-    			Serial.println(F("Now Testing 12V Solenoid Valve - 3/4''"));
-    		else if(c == '5') 
-    			Serial.println(F("Now Testing Water Flow Sensor G1/2''"));
-            else
-            {
-                Serial.println(F("illegal input!"));
-                return 0;
-            }
-            time0 = millis();
-            return c;
-            }
-        }
-    }
-
-/*******************************************************
-
-*    Circuito.io is an automatic generator of schematics and code for off
-*    the shelf hardware combinations.
-
-*    Copyright (C) 2016 Roboplan Technologies Ltd.
-
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*    In addition, and without limitation, to the disclaimers of warranties 
-*    stated above and in the GNU General Public License version 3 (or any 
-*    later version), Roboplan Technologies Ltd. ("Roboplan") offers this 
-*    program subject to the following warranty disclaimers and by using 
-*    this program you acknowledge and agree to the following:
-*    THIS PROGRAM IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS, AND 
-*    WITHOUT WARRANTIES OF ANY KIND EITHER EXPRESS OR IMPLIED.  ROBOPLAN 
-*    HEREBY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT 
-*    NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, TITLE, FITNESS 
-*    FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND THOSE ARISING BY 
-*    STATUTE OR FROM A COURSE OF DEALING OR USAGE OF TRADE. 
-*    YOUR RELIANCE ON, OR USE OF THIS PROGRAM IS AT YOUR SOLE RISK.
-*    ROBOPLAN DOES NOT GUARANTEE THAT THE PROGRAM WILL BE FREE OF, OR NOT 
-*    SUSCEPTIBLE TO, BUGS, SECURITY BREACHES, OR VIRUSES. ROBOPLAN DOES 
-*    NOT WARRANT THAT YOUR USE OF THE PROGRAM, INCLUDING PURSUANT TO 
-*    SCHEMATICS, INSTRUCTIONS OR RECOMMENDATIONS OF ROBOPLAN, WILL BE SAFE 
-*    FOR PERSONAL USE OR FOR PRODUCTION OR COMMERCIAL USE, WILL NOT 
-*    VIOLATE ANY THIRD PARTY RIGHTS, WILL PROVIDE THE INTENDED OR DESIRED
-*    RESULTS, OR OPERATE AS YOU INTENDED OR AS MAY BE INDICATED BY ROBOPLAN. 
-*    YOU HEREBY WAIVE, AGREE NOT TO ASSERT AGAINST, AND RELEASE ROBOPLAN, 
-*    ITS LICENSORS AND AFFILIATES FROM, ANY CLAIMS IN CONNECTION WITH ANY OF 
-*    THE ABOVE. 
-********************************************************/
